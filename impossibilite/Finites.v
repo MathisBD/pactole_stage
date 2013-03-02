9f43149b0395e8e997e6e4e4ea7edf7076dd968d
auger_cedric <auger_cedric@ce32948f-f1dc-422d-a153-26550fe5b315>
2013-03-02 09:30:21 +0000

    dernier ajout dans Finites.v : bounded. Je pense qu'il n'y a plus rien a y ajouter
    git-svn-id: https://gforge.cnam.fr/svn/pactole/trunk@13 ce32948f-f1dc-422d-a153-26550fe5b315

--- impossibilite/Finites.v
+++ impossibilite/Finites.v
@@ -27,6 +27,10 @@ Definition prev2 (f : finite) (x : name f) : prev_result f x :=
  | None => @FirstOne f x
  end (eq_refl _).
 
+Lemma SurjToInj A B (f : A -> B) (g : B -> A)
+: (forall x, f (g x) = x) -> (forall x y, g x = g y -> x = y).
+Proof. intros H x y Heq. case (H x), (H y). case Heq. split. Qed.
+
 Lemma NextSurj (f : finite) : forall x, next f (prev f x) = x.
 Proof. exact (fun x => proj2 (NextPrev f (prev f x) x) (eq_refl _)). Qed.
 
@@ -34,10 +38,10 @@ Lemma PrevSurj (f : finite) : forall x, prev f (next f x) = x.
 Proof. exact (fun x => proj1 (NextPrev f x (next f x)) (eq_refl _)). Qed.
 
 Lemma NextInj (f : finite) : forall x y, next f x = next f y -> x = y.
-Proof. intros; case (PrevSurj f y); case H; symmetry; apply PrevSurj. Qed.
+Proof. apply SurjToInj with (prev f). apply PrevSurj. Qed.
 
 Lemma PrevInj (f : finite) : forall x y, prev f x = prev f y -> x = y.
-Proof. intros; case (NextSurj f y); case H; symmetry; apply NextSurj. Qed.
+Proof. apply SurjToInj with (next f). apply NextSurj. Qed.
 
 Definition index_aux (f : finite) : forall x, Acc (NextRel f) x -> nat :=
   fix index x (H : Acc (NextRel f) x) :=
@@ -47,50 +51,78 @@ Definition index_aux (f : finite) : forall x, Acc (NextRel f) x -> nat :=
     S (index y (let (rec) := H in rec y (proj2 (NextPrev f _ _) Hy)))
   end.
 
-Definition index (f : finite) (x : name f) : nat := index_aux (RecNext f x).
+Definition index (f : finite) (x : option (name f)) : nat :=
+ match x with
+ | None => O
+ | Some x => S (index_aux (RecNext f x))
+ end.
 
 Definition coindex (f : finite) : nat -> option (name f) :=
  fix coindex n :=
  match n with
- | O => next f None
- | S n => match coindex n with
-          | Some m => next f (Some m)
-          | None => None
-          end
+ | O => None
+ | S n => next f (coindex n)
  end.
 
-Lemma nat_split (f : finite) : forall n, coindex f (index f n) = Some n.
+Lemma CoindexSurj_aux (f : finite)
+: forall n Hn, next f (coindex f (@index_aux f n Hn)) = Some n.
 Proof.
- unfold index.
- intros n; generalize (RecNext f n); revert n.
  fix 2; intros n [Hind]; simpl.
  destruct (prev2 f n) as [m Hm|H]; simpl.
- + rewrite nat_split; unfold PrevRel in Hm; clear - Hm.
+ + rewrite CoindexSurj_aux; unfold PrevRel in Hm; clear - Hm.
    now apply (proj2 (NextPrev f _ _)).
  + unfold First in H; clear - H.
    now apply (proj2 (NextPrev f _ _)).
 Qed.
 
-Lemma nat_cosplit (f : finite)
-: forall n, match coindex f n with
-            | Some k => index f k = n
-            | None => True
-            end.
+Lemma CoindexSurj (f : finite) : forall n, coindex f (index f n) = n.
+Proof. unfold index; intros [n|]; simpl; auto; apply CoindexSurj_aux. Qed.
+
+Lemma IndexInj_aux f x (Hx : Acc (NextRel f) x) y (Hy : Acc (NextRel f) y)
+: index_aux Hx = index_aux Hy -> x = y.
 Proof.
- intros n; case_eq (coindex f n); auto.
- unfold index; intros n0; generalize (RecNext f n0); revert n0.
- induction n; intros m [rec]; simpl.
- + destruct (prev2 f m); auto.
-   intros H; exfalso; revert p; unfold PrevRel; case H; clear.
-   rewrite PrevSurj; discriminate.
- + destruct (coindex f n); try discriminate.
-   destruct (prev2 f m); simpl.
-   - generalize (rec y (proj2 (NextPrev f (Some y) (Some m)) p)); intros.
-     f_equal; apply IHn.
-     clear - H p.
-     case (proj1 (NextPrev f (Some n0) (Some m)) H); auto.
-   - unfold First in f0.
-     case (proj2 (NextPrev f None (Some m)) f0); clear.
-     intros H; generalize (NextInj f _ _ H); clear.
-     discriminate.
+  intros H.
+  change y with (match Some y with Some y => y | _ => y end).
+  case (CoindexSurj_aux Hy).
+  change x with (match Some x with Some y => y | _ => y end).
+  case (CoindexSurj_aux Hx).
+  case H; split.
+Qed.
+
+Lemma IndexInj (f : finite) : forall x y, index f x = index f y -> x = y.
+Proof. apply SurjToInj with (coindex f). apply CoindexSurj. Qed.
+
+Definition card (f : finite) : nat := index f (prev f None).
+
+Lemma bounded (f : finite) : forall x, index f x <= card f.
+Proof.
+  unfold card, index; intros x.
+  case_eq (prev f None).
+  * intros n Hn; case x; clear x; [|apply Le.le_0_n].
+    intros x; apply Le.le_n_S.
+    generalize (RecNext f x); revert x.
+    fix 2; intros x [rec]; simpl.
+    generalize (fun x Hx => bounded x (rec x Hx)).
+    clear bounded; simpl.
+    destruct (prev2 f x); [|intros; apply Le.le_0_n].
+    generalize (proj2 (NextPrev f (Some y) (Some x)) p).
+    intros acc Hacc; generalize (rec y acc), (Hacc _ acc); clear - p acc Hn.
+    unfold PrevRel in p; intros; simpl in *.
+    generalize (IndexInj_aux a (RecNext f n)).
+    destruct H as [|o Ho]; [|intros _; apply Le.le_n_S; auto].
+    intros H; exfalso.
+    revert Hn; case (H (eq_refl _)); case p; clear.
+    intros H; generalize (PrevInj f None (Some x) H); clear.
+    intros H; change (match Some x with None => True | _ => False end).
+    destruct H; split.
+  * destruct x; [|left].
+    intros H; exfalso.
+    generalize (RecNext f n); revert n.
+    fix 2; intros x [rec].
+    destruct (prev2 f x).
+    + exact (bounded y (rec y (proj2 (NextPrev f (Some y) (Some x)) p))).
+    + unfold First in f0; revert f0; destruct H; subst o; clear.
+      intros K; generalize (PrevInj f (Some x) None K); clear.
+      intros H; change (match @None (name f) with None => False |_ => True end).
+      destruct H; split.
 Qed.
