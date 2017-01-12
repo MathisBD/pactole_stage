dc423c2655f5382027ab56d00984cb1fcf784afc
Lionel Rieg <lionel.rieg@yale.edu>
2017-01-12 15:43:45 -0500

    Préparation du nouveau package

--- current-formalism/build_package.sh
+++ current-formalism/build_package.sh
@@ -2,6 +2,7 @@
 
 rm -rf ./package
 mkdir ./package
+mkdir ./package/MMaps
 mkdir ./package/MMultiset
 mkdir ./package/Convergence
 mkdir ./package/Gathering
@@ -9,6 +10,8 @@ mkdir ./package/Gathering/InR
 mkdir ./package/Gathering/InR2
 
 coq_makefile -f _CoqProject -o makefile
+
+cp -r MMaps/MMapInterface.v MMaps/MMapFacts.v MMaps/MMapWeakList.v ./package/MMaps/
 
 cp -r MMultiset/Preliminary.v MMultiset/MMultisetInterface.v MMultiset/MMultisetFacts.v MMultiset/MMultisetWMap.v MMultiset/MMultisetExtraOps.v ./package/MMultiset/
 
@@ -20,7 +23,7 @@ cp Gathering/Definitions.v Gathering/FlexDefinitions.v ./package/Gathering/
 
 cp Gathering/InR/Rcomplements.v Gathering/InR/Algorithm.v Gathering/InR/Impossibility.v ./package/Gathering/InR/
 
-cp Gathering/InR2/R2geometry.v Gathering/InR2/Algorithm.v Gathering/InR2/FSyncFlexNoMultAlgorithm.v ./package/Gathering/InR2
+cp Gathering/InR2/R2geometry.v Gathering/InR2/Algorithm.v Gathering/InR2/FSyncFlexNoMultAlgorithm.v ./package/Gathering/InR2/
 
 make -C package -j 3 Gathering/InR/Algorithm.vo Gathering/InR/Impossibility.vo Gathering/InR2/Algorithm.vo Gathering/InR2/FSyncFlexNoMultAlgorithm.vo Convergence/Impossibility_2G_1B.vo
 make -C package clean
