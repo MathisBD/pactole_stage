4946a1e7a7fccca7445c0babd1b505062e678786
Lionel Rieg <lionel.rieg@yale.edu>
2017-01-23 17:42:37 -0500

    Fixing the script to generate the package

--- current-formalism/build_package.sh
+++ current-formalism/build_package.sh
@@ -19,7 +19,7 @@ cp Preliminary.v Lexprod.v Robots.v Configurations.v Streams.v CommonFormalism.v
 
 cp Convergence/Impossibility_2G_1B.v ./package/Convergence/
 
-cp Gathering/Definitions.v Gathering/FlexDefinitions.v ./package/Gathering/
+cp Gathering/Definitions.v Gathering/FlexDefinitions.v Gathering/Impossibility.v ./package/Gathering/
 
 cp Gathering/InR/Rcomplements.v Gathering/InR/Algorithm.v Gathering/InR/Impossibility.v ./package/Gathering/InR/
 
