210dcaaacfd3f9e620aab1fd530dc44005e6d726
Lionel Rieg <lionel.rieg@yale.edu>
2017-08-28 12:44:15 -0400

    Fin des preuves dans FMaps

--- current-formalism/Util/FMaps/FMapList.v
+++ current-formalism/Util/FMaps/FMapList.v
@@ -2,6 +2,7 @@
 
 Require Import RelationPairs.
 Require Import List SetoidList.
+Require Import Bool.
 Require Import SetoidDec.
 Require Pactole.Util.Preliminary.
 Require Import Pactole.Util.FMaps.FMapInterface.
@@ -162,10 +163,7 @@ Qed.
 Definition t_mapi {elt'} (f : key -> elt -> elt') (s : tt elt) : tt elt' :=
   exist _ (list_mapi f (proj1_sig s)) (t_mapi_lemma _ (proj2_sig s)).
 
-
 End ListOperations.
-
-(* Local Notation tt elt := (sig (@NoDupA (key * elt) (equiv@@1))). *)
 
 
 Instance MapList key `{EqDec key} : FMap := {|
@@ -191,150 +189,232 @@ Local Transparent dict MapsTo empty is_empty mem add find remove equal map mapi 
 Local Notation t := (sig (@NoDupA (_ * _) (equiv@@1))).
 Local Notation eq_pair := (fun xn yp => fst xn == fst yp /\ snd xn = snd yp).
 
-Instance MapListFacts key `{EqDec key} : FMapSpecs (MapList _).
+Instance MapListFacts_MapsTo key `{EqDec key} : FMapSpecs_MapsTo (MapList _).
+Proof.
+split. intros elt [m Hm] x y e Hxy Hx. simpl in *.
+induction m; inversion_clear Hx.
+- left. simpl in *. now rewrite <- Hxy.
+- right. inversion_clear Hm. now apply IHm.
+Qed.
+
+Instance MapListFacts_mem key `{EqDec key} : FMapSpecs_mem (MapList _).
 Proof. split.
-* (* MapsTo *)
-  split; simpl.
-  + intros elt [m Hm] x y e Hxy Hx. simpl in *. induction m; inversion_clear Hx.
-    - left. simpl in *. now rewrite <- Hxy.
-    - right. inversion_clear Hm. now apply IHm.
-* (* mem *)
-  split.
-  + intros elt [m Hm] x [e Hin]. simpl in *. induction m as [| [y n] l]; inversion_clear Hin.
-    - simpl. destruct (equiv_dec x y) as [Hxy | Hxy]; trivial. elim Hxy. now destruct H0.
-    - simpl. destruct (equiv_dec x y) as [Hxy | Hxy]; trivial. inversion_clear Hm. auto.
-  + intros elt [m Hm] x Hmem. unfold In. simpl in *. induction m as [| [y n] l]; simpl in Hmem.
-    - discriminate.
-    - destruct (equiv_dec x y) as [Hxy | Hxy].
-      -- exists n. left. now split.
-      -- inversion_clear Hm. apply IHl in Hmem; trivial; []. destruct Hmem as [e ?]. exists e. now right.
-* (* empty *)
-  split.
-  + intros elt x e Hin. inversion Hin.
-* (* is_empty *)
-  split.
-  + intros elt [m Hm] Hm'. destruct m as [| [x n] l]; trivial.
-    elim Hm' with x n. now left.
-  + intros elt [m Hm] Hm'. destruct m as [| [x n] l]; try discriminate.
-    intros x n Hin. inversion Hin.
-* (* add *)
-  split.
-  + intros elt [m Hm] x y e Hxy. simpl in *. induction m as [| [z p] l]; simpl.
-    - now left.
-    - inversion_clear Hm. destruct (equiv_dec x z); now left + right; auto.
-  + intros elt [m Hm] x y e e' Hxy Hy. simpl in *. induction m as [| [z p] l].
-    - inversion Hy.
-    - simpl. destruct (equiv_dec x z).
-      -- right. inversion_clear Hy; trivial.
-         elim Hxy. destruct H0. now transitivity z.
-      -- inversion_clear Hm. inversion_clear Hy; now left + (right; apply IHl).
-  + intros elt [m Hm] x y e e' Hxy Hy. simpl in *. induction m as [| [z p] l].
-    - inversion_clear Hy; inversion H0. now elim Hxy.
-    - simpl in *. destruct (equiv_dec x z).
-      -- right. inversion_clear Hy; trivial. now elim Hxy.
-      -- inversion_clear Hm. inversion_clear Hy; now left + (right; apply IHl).
-* (* remove *)
-  split.
-  + intros elt [m Hm] x y Hxy. simpl. unfold t_remove, In. simpl. induction m as [| [z p] l].
-    - simpl. intros [? Habs]. inversion Habs.
-    - simpl. inversion_clear Hm. destruct (equiv_dec x z) as [Hxz | Hxz]; auto; [].
-      intros [n Habs]. inversion_clear Habs.
-      -- elim Hxz. destruct H2. now transitivity y.
-      -- apply IHl; eauto.
-  + intros elt [m Hm] x y e Hxy Hy. simpl in *. induction m as [| [z p] l].
-    - inversion Hy.
-    - inversion_clear Hm. simpl. destruct (equiv_dec x z).
-      -- inversion_clear Hy; simpl in *; auto; [].
-         elim Hxy. destruct H2. now transitivity z.
-      -- inversion_clear Hy; now left + (right; apply IHl).
-  + intros elt [m Hm] x y e Hy. simpl in *. induction m as [| [z p] l].
-    - inversion_clear Hy.
-    - inversion_clear Hm. simpl in *. destruct (equiv_dec x z); auto.
-      inversion_clear Hy; now left + (right; apply IHl).
-* (* find *)
-  split.
-  + intros elt [m Hm] x e Hin. simpl in *. induction m as [| [y p] l].
-    - inversion Hin.
-    - inversion_clear Hm. simpl. destruct (equiv_dec x y).
-      -- inversion_clear Hin; try (now f_equal); [].
-         assert (Heq : equiv@@1 (x, e) (y, p)) by assumption.
+* intros elt [m Hm] x [e Hin]. simpl in *. induction m as [| [y n] l]; inversion_clear Hin.
+  + simpl. destruct (equiv_dec x y) as [Hxy | Hxy]; trivial. elim Hxy. now destruct H0.
+  + simpl. destruct (equiv_dec x y) as [Hxy | Hxy]; trivial. inversion_clear Hm. auto.
+* intros elt [m Hm] x Hmem. unfold In. simpl in *. induction m as [| [y n] l]; simpl in Hmem.
+  + discriminate.
+  + destruct (equiv_dec x y) as [Hxy | Hxy].
+    - exists n. left. now split.
+    - inversion_clear Hm. apply IHl in Hmem; trivial; []. destruct Hmem as [e ?]. exists e. now right.
+Qed.
+
+Instance MapListFacts_empty key `{EqDec key} : FMapSpecs_empty (MapList _).
+Proof. split. intros elt x e Hin. inversion Hin. Qed.
+
+Instance MapListFacts_is_empty key `{EqDec key} : FMapSpecs_is_empty (MapList _).
+Proof. split.
+* intros elt [m Hm] Hm'. destruct m as [| [x n] l]; trivial.
+  elim Hm' with x n. now left.
+* intros elt [m Hm] Hm'. destruct m as [| [x n] l]; try discriminate.
+  intros x n Hin. inversion Hin.
+Qed.
+
+Instance MapListFacts_add key `{EqDec key} : FMapSpecs_add (MapList _).
+Proof. split.
+* intros elt [m Hm] x y e Hxy. simpl in *. induction m as [| [z p] l]; simpl.
+  + now left.
+  + inversion_clear Hm. destruct (equiv_dec x z); now left + right; auto.
+* intros elt [m Hm] x y e e' Hxy Hy. simpl in *. induction m as [| [z p] l].
+  + inversion Hy.
+  + simpl. destruct (equiv_dec x z).
+    - right. inversion_clear Hy; trivial.
+      elim Hxy. destruct H0. now transitivity z.
+    - inversion_clear Hm. inversion_clear Hy; now left + (right; apply IHl).
+* intros elt [m Hm] x y e e' Hxy Hy. simpl in *. induction m as [| [z p] l].
+  + inversion_clear Hy; inversion H0. now elim Hxy.
+  + simpl in *. destruct (equiv_dec x z).
+    - right. inversion_clear Hy; trivial. now elim Hxy.
+    - inversion_clear Hm. inversion_clear Hy; now left + (right; apply IHl).
+Qed.
+
+Instance MapListFacts_remove key `{EqDec key} : FMapSpecs_remove (MapList _).
+Proof. split.
+* intros elt [m Hm] x y Hxy. simpl. unfold t_remove, In. simpl. induction m as [| [z p] l].
+  + simpl. intros [? Habs]. inversion Habs.
+  + simpl. inversion_clear Hm. destruct (equiv_dec x z) as [Hxz | Hxz]; auto; [].
+    intros [n Habs]. inversion_clear Habs.
+    - elim Hxz. destruct H2. now transitivity y.
+    - apply IHl; eauto.
+* intros elt [m Hm] x y e Hxy Hy. simpl in *. induction m as [| [z p] l].
+  + inversion Hy.
+  + inversion_clear Hm. simpl. destruct (equiv_dec x z).
+    - inversion_clear Hy; simpl in *; auto; [].
+      elim Hxy. destruct H2. now transitivity z.
+    - inversion_clear Hy; now left + (right; apply IHl).
+* intros elt [m Hm] x y e Hy. simpl in *. induction m as [| [z p] l].
+  + inversion_clear Hy.
+  + inversion_clear Hm. simpl in *. destruct (equiv_dec x z); auto.
+    inversion_clear Hy; now left + (right; apply IHl).
+Qed.
+
+Instance MapListFacts_find key `{EqDec key} : FMapSpecs_find (MapList _).
+Proof. split.
+* intros elt [m Hm] x e Hin. simpl in *. induction m as [| [y p] l].
+  + inversion Hin.
+  + inversion_clear Hm. simpl. destruct (equiv_dec x y).
+    - inversion_clear Hin; try (now f_equal); [].
+      assert (Heq : equiv@@1 (x, e) (y, p)) by assumption.
+      elim H0. rewrite <- Heq. revert H2.
+      apply Pactole.Util.Preliminary.InA_impl_compat; trivial; []. now repeat intro.
+    - inversion_clear Hin; now auto.
+* intros elt [m Hm] x e Hin. simpl in *. induction m as [| [y p] l].
+  + inversion Hin.
+  + inversion_clear Hm. simpl in Hin. destruct (equiv_dec x y).
+    - inversion_clear Hin; now auto.
+    - right. auto.
+Qed.
+
+Instance MapListFacts_elements key `{EqDec key} : FMapSpecs_elements (MapList _).
+Proof. split.
+* tauto.
+* tauto.
+* intros elt [m Hm]. simpl. assumption.
+Qed.
+
+Instance MapListFacts_cardinal key `{EqDec key} : FMapSpecs_cardinal (MapList _).
+Proof. split. tauto. Qed.
+
+Instance MapListFacts_fold key `{EqDec key} : FMapSpecs_fold (MapList _).
+Proof. split.
+intros elt [m Hm] A i f. simpl. revert i. induction m as [| [y p] l]; simpl.
+- reflexivity.
+- intro i. inversion_clear Hm. now rewrite IHl.
+Qed.
+
+
+(* TODO *)
+Definition Submap key `{EqDec key} {elt : Type} cmp (m m' : Map [key, elt]) :=
+  (forall k, In k m -> In k m') /\
+  (forall k e e', MapsTo k e m -> MapsTo k e' m' -> cmp e e' = true).
+
+Local Lemma submap_1 key elt `{EqDec key} : forall m m' cmp,
+  Submap cmp m m' -> list_submap (elt:=elt) cmp (proj1_sig m) (proj1_sig m') = true.
+Proof.
+unfold Submap, list_submap.
+intros [m Hm].
+induction m as [| [k e] m].
+* simpl; auto.
+* simpl; intros m' cmp [H1 H2].
+  inversion_clear Hm.
+  assert (Hin : In k m') by (apply H1; exists e; simpl; auto).
+  destruct Hin as (e', Hin).
+  unfold list_check at 2. apply find_1 in Hin. simpl in Hin. rewrite Hin.
+  rewrite (H2 k); simpl; auto.
+  + apply (IHm H3).
+    split; intuition.
+    - apply H1. destruct H4 as [e'' H4]; exists e''; simpl in *; auto.
+    - apply H2 with k0; auto.
+  + change (MapsTo k e' m'). now apply find_2.
+Qed.
+
+Local Lemma submap_2 key elt `{EqDec key} : forall m m' cmp,
+  list_submap (elt:=elt) cmp (proj1_sig m) (proj1_sig m') = true -> Submap cmp m m'.
+Proof.
+unfold Submap, list_submap.
+intros [m Hm]. induction m as [| [k e] m].
+* simpl in *; auto.
+  split; intros * Hin.
+  + destruct Hin. inversion H1.
+  + inversion Hin.
+* simpl; intros * Hsub.
+  inversion_clear Hm.
+  rewrite andb_b_true in Hsub.
+  assert (Hcheck : list_check cmp k e (proj1_sig m') = true).
+  { clear H1 H0 IHm.
+    set (b := list_check cmp k e (proj1_sig m')) in *.
+    revert Hsub. generalize b; clear b.
+    induction m as [| [y p] m]; simpl; auto; intros; simpl in *.
+    assert (Hnodup : NoDupA (equiv @@1) ((k, e) :: m)).
+    { inversion_clear Hm. inversion_clear H1. constructor; auto. }
+    destruct (andb_prop _ _ (IHm Hnodup _ Hsub)); auto. }
+  rewrite Hcheck in Hsub.
+  destruct (IHm H1 m' cmp Hsub) as [H2 H3]; auto.
+  unfold list_check in Hcheck.
+  case_eq (list_find k (proj1_sig m')); [intros e' H4 | intros H4];
+    rewrite H4 in Hcheck; try discriminate.
+  split; intros.
+  + destruct H5 as [e0 H6]; inversion_clear H6.
+    - compute in H5; destruct H5; subst.
+      exists e'.
+      apply MapsTo_1 with k; auto.
+      apply find_2; auto.
+    - apply H2. exists e0; auto.
+  + inversion_clear H5.
+    - compute in H7; destruct H7; subst.
+      change (MapsTo k0 e'0 m') in H6. apply (MapsTo_1 H5), find_1 in H6.
+      simpl in H6. congruence.
+    - apply H3 with k0; auto.
+Qed.
+
+
+Instance MapListFacts_equal key `{EqDec key} : FMapSpecs_equal (MapList _).
+Proof. split.
+* unfold Equivb, equal.
+  intuition.
+  apply andb_true_intro; split; apply submap_1; unfold Submap; firstorder.
+* unfold Equivb, equal.
+  intros elt m m' cmp Heq.
+  destruct (andb_prop _ _ Heq); clear Heq.
+  generalize (submap_2 m m' cmp H0).
+  generalize (submap_2 m' m _ H1).
+  firstorder.
+Qed.
+
+Instance MapListFacts_map key `{EqDec key} : FMapSpecs_map (MapList _).
+Proof. split.
+* intros elt elt' [m Hm] x e f Hin. simpl in *. induction m as [| [y p] m].
+  + inversion Hin.
+  + inversion_clear Hm. simpl. destruct (equiv_dec x y).
+    - left. simpl. split; trivial. inversion_clear Hin.
+      -- simpl in *. destruct H2. now subst.
+      -- assert (Heq : equiv@@1 (x, e) (y, p)) by assumption.
          elim H0. rewrite <- Heq. revert H2.
          apply Pactole.Util.Preliminary.InA_impl_compat; trivial; []. now repeat intro.
-      -- inversion_clear Hin; now auto.
-  + intros elt [m Hm] x e Hin. simpl in *. induction m as [| [y p] l].
-    - inversion Hin.
-    - inversion_clear Hm. simpl in Hin. destruct (equiv_dec x y).
-      -- inversion_clear Hin; now auto.
-      -- right. auto.
-* (* elements *)
-  split.
-  + tauto.
-  + tauto.
-  + intros elt [m Hm]. simpl. assumption.
-* (* cardinal *)
-  split.
-  + tauto.
-* (* fold *)
-  split.
-  + intros elt [m Hm] A i f. simpl. revert i. induction m as [| [y p] l]; simpl.
-    - reflexivity.
-    - intro i. inversion_clear Hm. now rewrite IHl.
-* (* equal *)
-(*   admit. *)
-  split.
-  + unfold Equivb, Equiv, In. intros elt [m Hm] [m' Hm'] cmp. simpl.
-    revert m' Hm'. induction m as [| [y p] l]; simpl; intros m' Hm' Heq.
-    - assert (m' = nil).
-      { destruct m' as [| [k x] m']; trivial. destruct Heq as [Heq _].
-        assert (Habs : exists e : elt, InA eq_pair (k, e) ((k, x) :: m')) by now exists x; left.
-        rewrite <- Heq in Habs. destruct Habs as [? Habs]. inversion Habs. }
-      subst. reflexivity.
-    - assert (Heqm' : m' <> nil).
-      { destruct m' as [| [k x] m']; try discriminate; []. destruct Heq as [Heq _].
-        assert (Habs : exists e : elt, InA eq_pair (y, e) ((y, p) :: l)) by now exists p; left.
-        rewrite Heq in Habs. destruct Habs as [? Habs]. inversion Habs. }
-      destruct m' as [| [x n] m']; try now elim Heqm'.
-      unfold list_equal. simpl. admit.
-  + admit.
-* (* map *)
-  split.
-  + intros elt elt' [m Hm] x e f Hin. simpl in *. induction m as [| [y p] m].
-    - inversion Hin.
-    - inversion_clear Hm. simpl. destruct (equiv_dec x y).
-      ++ left. simpl. split; trivial. inversion_clear Hin.
-         -- simpl in *. destruct H2. now subst.
-         -- assert (Heq : equiv@@1 (x, e) (y, p)) by assumption.
-            elim H0. rewrite <- Heq. revert H2.
-            apply Pactole.Util.Preliminary.InA_impl_compat; trivial; []. now repeat intro.
-      ++ inversion_clear Hin; try easy; [].
-         right. auto.
-  + unfold In. intros elt elt' [m Hm] x f [e Hin]. simpl in *. induction m as [| [y p] m].
-    - inversion Hin.
-    - inversion_clear Hm. simpl. destruct (equiv_dec x y).
-      ++ exists p. now left.
-      ++ inversion_clear Hin; try easy; [].
-         destruct IHm as [e' ?]; trivial; [].
-         exists e'. now right.
-* (* mapi *)
-  split.
-  + intros elt elt' [m Hm] x e f Hin. simpl in *. induction m as [| [y p] m].
-    - inversion Hin.
-    - inversion_clear Hm. simpl. destruct (equiv_dec x y).
-      ++ exists y. split; try (now symmetry); []. left.
-         simpl. split; trivial. inversion_clear Hin.
-         -- simpl in *. destruct H2. now subst.
-         -- assert (Heq : equiv@@1 (x, e) (y, p)) by assumption.
-            elim H0. rewrite <- Heq. revert H2.
-            apply Pactole.Util.Preliminary.InA_impl_compat; trivial; []. now repeat intro.
-      ++ inversion_clear Hin; try easy; [].
-         destruct IHm as [y' Hy']; trivial; []. exists y'.
-         split; try (now symmetry); []. now right.
-  + unfold In. intros elt elt' [m Hm] x f [e Hin]. simpl in *. induction m as [| [y p] m].
-    - inversion Hin.
-    - inversion_clear Hm. destruct (equiv_dec x y).
-      ++ exists p. now left.
-      ++ inversion_clear Hin; try easy; [].
-         destruct IHm as [e' ?]; trivial; [].
-         exists e'. now right.
-Admitted.
+    - inversion_clear Hin; try easy; [].
+      right. auto.
+* unfold In. intros elt elt' [m Hm] x f [e Hin]. simpl in *. induction m as [| [y p] m].
+  + inversion Hin.
+  + inversion_clear Hm. simpl. destruct (equiv_dec x y).
+    - exists p. now left.
+    - inversion_clear Hin; try easy; [].
+      destruct IHm as [e' ?]; trivial; [].
+      exists e'. now right.
+Qed.
 
+Instance MapListFacts_mapi key `{EqDec key} : FMapSpecs_mapi (MapList _).
+Proof. split.
+* intros elt elt' [m Hm] x e f Hin. simpl in *. induction m as [| [y p] m].
+  + inversion Hin.
+  + inversion_clear Hm. simpl. destruct (equiv_dec x y).
+    - exists y. split; try (now symmetry); []. left.
+      simpl. split; trivial. inversion_clear Hin.
+      -- simpl in *. destruct H2. now subst.
+      -- assert (Heq : equiv@@1 (x, e) (y, p)) by assumption.
+         elim H0. rewrite <- Heq. revert H2.
+         apply Pactole.Util.Preliminary.InA_impl_compat; trivial; []. now repeat intro.
+    - inversion_clear Hin; try easy; [].
+      destruct IHm as [y' Hy']; trivial; []. exists y'.
+      split; try (now symmetry); []. now right.
+* unfold In. intros elt elt' [m Hm] x f [e Hin]. simpl in *. induction m as [| [y p] m].
+  + inversion Hin.
+  + inversion_clear Hm. destruct (equiv_dec x y).
+    - exists p. now left.
+    - inversion_clear Hin; try easy; [].
+      destruct IHm as [e' ?]; trivial; [].
+      exists e'. now right.
+Qed.
+
+(* The full set of specifications. *)
+Instance MapListFacts key `{EqDec key} : FMapSpecs (MapList _).
+Proof. split; auto with typeclass_instances. Qed.
